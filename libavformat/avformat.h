5f2e1dbb090320dd08f0f4c413ed620d6250013c
chenfengfeng123 <chenfengfeng@turingvideo.net>
2024-07-30 19:30:41 +0800

    code format

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -2908,10 +2908,10 @@ AVRational av_stream_get_codec_timebase(const AVStream *st);
 /**
  * Set parameter for rtsp stream.
  *
- * @param s     media file handle
- * @param uri      rtsp url
- * @param headers      rtsp headers
- * @param content  rtsp body content
+ * @param smedia     file handle
+ * @param uri     rtsp url
+ * @param headers     rtsp headers
+ * @param content     rtsp body content
  * @return 0 if OK, -1 on format error
  */
 int av_rtsp_send_set_parameter(AVFormatContext *s, const char *uri, const char *headers, const char *content);
